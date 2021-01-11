1bbca7f097e29deef899e4be232c83d71e70fc2d
Dere <Dere@DESKTOP-QFUG7FB>
2021-01-11 17:08:18 +0300

    FuelCardReport cs

---
+++ Chai.WorkflowManagment.CoreDomain/Report/FuelCardReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chai.WorkflowManagment.CoreDomain.Report
+{
+    [ComplexType]
+    public class FuelCardReport
+    {
+        public DateTime Date { get; set; }
+
+        public string Card_Holder_Name { get; set; }
+
+        public string Year { get; set; }
+
+        public string Month { get; set; }
+
+        private Decimal Total_Reimbursement { get; set; }
+
+        public string Vehicle_Reg_Number { get; set; }
+    }
+}
