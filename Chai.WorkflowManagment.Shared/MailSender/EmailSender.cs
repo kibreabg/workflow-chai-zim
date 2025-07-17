eb6b7461a2e600cffd8bb42cf6e7d23b56e1065b
Kibreab Gizatu <kibreabg@gmail.com>
2025-07-18 00:27:08 +0300

    Refactor EmailSender and update office/public links
    Changed EmailSender to a static class and improved link messaging in emails to distinguish between office and public access. Removed unused variables and cleaned up code formatting for clarity.

--- Chai.WorkflowManagment.Shared/MailSender/EmailSender.cs
+++ Chai.WorkflowManagment.Shared/MailSender/EmailSender.cs
@@ -1,22 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Net;
-using System.Net.Mail;
-using System.Net.Configuration;
 using System.Configuration;
+using System.Net;
+using System.Net.Configuration;
+using System.Net.Mail;
+
 namespace Chai.WorkflowManagment.Shared.MailSender
 {
-
-    public class EmailSender
+    public static class EmailSender
     {
-
         public static bool Send(string to, string subject, string body)
         {
-            string from = string.Empty;
             string localIP = "http://zimops/ZWFM/UserLogin";
-            //string publicIp = "http://197.211.216.65/ZWFM/UserLogin.aspx";
-        
+            string publicIp = "http://zimops.clintonhealthaccess.org:444/ZWFM/UserLogin.aspx";
 
             SmtpSection section = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
 
@@ -28,10 +23,9 @@ namespace Chai.WorkflowManagment.Shared.MailSender
                     client.Timeout = 2000000;
                     client.UseDefaultCredentials = false;
                     client.Credentials = new NetworkCredential(section.Network.UserName, section.Network.Password);
-                    client.Send(section.From, to, subject, body + " Click here: " + localIP );
+                    client.Send(section.From, to, subject, body + " Click here if you're in the office: " + localIP + " or Click here if you're outside the office: " + publicIp);
                     client.Dispose();
                 }
-
             }
             catch (Exception ex)
             {
@@ -44,7 +38,6 @@ namespace Chai.WorkflowManagment.Shared.MailSender
 
         public static bool SendEmails(string from, string to, string subject, string body)
         {
-
             SmtpSection section = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
 
             try
@@ -58,7 +51,6 @@ namespace Chai.WorkflowManagment.Shared.MailSender
                     client.Send(section.From, to, subject, body);
                     client.Dispose();
                 }
-
             }
             catch (Exception ex)
             {
@@ -71,22 +63,21 @@ namespace Chai.WorkflowManagment.Shared.MailSender
 
         public static bool SendException(string to, string subject, string body)
         {
-            string from = string.Empty;
-
             SmtpSection section = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
 
             //create the mail message
-            MailMessage mail = new MailMessage();
-
-            //set the addresses
-            mail.From = new MailAddress(section.From);
+            MailMessage mail = new MailMessage
+            {
+                //set the addresses
+                From = new MailAddress(section.From)
+            };
             mail.To.Add(to);
 
             //set the content
             mail.Subject = subject;
             mail.Body = body;
             mail.IsBodyHtml = true;
-            
+
             try
             {
                 using (SmtpClient client = new SmtpClient(section.Network.Host, section.Network.Port))
@@ -98,7 +89,6 @@ namespace Chai.WorkflowManagment.Shared.MailSender
                     client.Send(mail);
                     client.Dispose();
                 }
-
             }
             catch (Exception ex)
             {
@@ -108,7 +98,5 @@ namespace Chai.WorkflowManagment.Shared.MailSender
 
             return false;
         }
-
     }
 }
-
